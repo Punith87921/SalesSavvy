1ba70b316093a3bc2cd8587dbe8590d47eddf8fa
Punith87921 <punigowda87921@gmail.com>
2025-02-18 14:24:47 +0530

    Front end first commit

--- src/CategoryNavigation.jsx
+++ src/CategoryNavigation.jsx
@@ -1,21 +1,24 @@
-import React from 'react'; 
+// CategoryNavigation.jsx
+import React from 'react';
 import './assets/styles.css';
 
-export function CategoryNavigation({ onCategoryClick }) { 
-    // Static categories list 
-    const categories = ['Shirts', 'Pants', 'Accessories', 'Mobiles', 'Mobile Accessories'];
+export function CategoryNavigation({ onCategoryClick }) {
+  // Static categories list
+  const categories = ['Shirts', 'Pants', 'Accessories', 'Mobiles', 'Mobile Accessories'];
 
-    return ( 
-    <nav className="category-navigation"> 
-    <ul className="category-list"> 
+  return (
+    <nav className="category-navigation">
+      <ul className="category-list">
         {categories.map((category, index) => (
-            <li key={index} className="category-item" 
-            onClick={() => onCategoryClick(category)} // Trigger the click handler on category 
-            click >
-              {category} 
-              </li> 
-              ))} 
-              </ul> 
-              </nav> 
-            ); 
-        }  
+          <li
+            key={index}
+            className="category-item"
+            onClick={() => onCategoryClick(category)} // Trigger the click handler on category click
+          >
+            {category}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
